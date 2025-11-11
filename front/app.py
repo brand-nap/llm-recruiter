bff0b359edb6180b89753c00dabf4ca4a5fcffa2
Brandon Perez <bperez3@apple.com>
2025-11-10 22:26:52 -0700

    add reqs

--- front/app.py
+++ front/app.py
@@ -6,12 +6,6 @@ st.set_page_config(
     layout="centered"           
 )
 
-from wxai_langchain.llm import LangChainInterface
-from langchain_community.document_loaders import PyPDFLoader
-from langchain_community.vectorstores import FAISS
-from langchain_community.embeddings import HuggingFaceEmbeddings
-from langchain_text_splitters import RecursiveCharacterTextSplitter
-
 from xai_sdk import Client
 from xai_sdk.chat import user, system
 
